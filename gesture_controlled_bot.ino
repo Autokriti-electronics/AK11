a7104479287ab3426f195f35ac6417242e8aec95
SAE NIT Kurukshetra <43487886+Autokriti-electronics@users.noreply.github.com>
2019-09-22 15:14:32 +0530

    Add files via upload

---
+++ gesture_controlled_bot.ino
@@ -0,0 +1,83 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(10, 11); // RX | TX
+
+void setup() 
+{
+   pinMode(2, OUTPUT);  
+   pinMode(3, OUTPUT);
+   pinMode(4, OUTPUT);
+   pinMode(5, OUTPUT);
+  Serial.begin(9600);
+  BTSerial.begin(9600);  
+}
+
+void Forward(){
+    Serial.println("FORWARD");
+    digitalWrite(2,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(5,LOW);
+  }
+
+ void Backward()
+  { Serial.println("BACKWARD");
+    digitalWrite(2,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(5,HIGH);
+  }
+
+ void Left()
+  { Serial.println("LEFT");
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(5,LOW);
+  }
+
+ void Right()
+  { Serial.println("RIGHT");
+    digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+  }
+  
+void Stop()
+  
+  {Serial.println("STOP");
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+  }
+
+
+void loop()
+{ 
+  char comm;
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+  if (BTSerial.available())
+   { comm = BTSerial.read();
+    }
+  else comm = 'a';
+  Serial.println(comm);
+  switch(comm){
+    case '1':
+      Forward();
+      break;
+    case '2':
+      Backward();
+      break;
+    case '3':
+      Right();
+      break;
+    case '4':
+      Left();
+      break;
+    default:
+      Stop();
+      break; 
+    }
+  delay(50);
+}
