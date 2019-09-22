28a0137f78e20e4e207e88cd574dfbaeb0c8875a
SAE NIT Kurukshetra <43487886+Autokriti-electronics@users.noreply.github.com>
2019-09-22 10:21:34 +0530

    Add files via upload

---
+++ AT_mode.ino
@@ -0,0 +1,18 @@
+//Code for AT mode
+#include<SoftwareSerial.h>
+SoftwareSerial BT(10,11); //RX 11 and TX 10
+void setup ()
+{
+  Serial.begin(9600);
+  BT.begin(38400);
+ 
+}
+void loop()
+{
+  if(BT.available())
+  Serial.write(BT.read());
+  if(Serial.available())
+  BT.write(Serial.read());
+  
+}
+
